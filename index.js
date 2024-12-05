f214bf7784d94fe1771e39f87ee8028f37d113b6
bikesh23 <bikesh.chaudhary@citytech.global>
2024-12-05 10:52:32 +0545

    init

--- index.js
+++ index.js
@@ -9,9 +9,8 @@ onload = () => {
 
   // Define an array of texts to rotate through
   const texts = ["Hello World!", "Welcome!", "Good Day!", "Hola Mundo!", "Bonjour le monde!", "Ciao mondo!"];
+  
   let textIndex = 0;
-
-  // Set the text change interval to 2 seconds (2000 ms)
   setInterval(() => {
     helloWorldText.textContent = texts[textIndex]; // Change the text content
     textIndex = (textIndex + 1) % texts.length; // Loop through the array
